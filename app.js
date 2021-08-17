cb1f843381879aa0bcb5b1b2e78a3ebb2bbfc653
Miguel Bugalho <miguel.bugalho@gmail.com>
2021-08-17 17:27:01 +0100

    Added cors

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var customersRouter = require('./routes/customersRoutes');
 
 var app = express();
 
+// Enabling cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
